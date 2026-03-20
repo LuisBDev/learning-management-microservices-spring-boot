b0eb76144bb904d01787a981dabe18491056bcaa
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-20 14:41:47 -0500

    feat(identity): implement CustomUserDetails for user authentication

---
+++ identity-service-v1/src/main/java/com/lms/identity/security/CustomUserDetails.java
@@ -0,0 +1,71 @@
+package com.lms.identity.security;
+
+import com.lms.identity.infrastructure.persistence.entity.RoleEntity;
+import com.lms.identity.infrastructure.persistence.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity user;
+
+    public CustomUserDetails(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        Set<RoleEntity> roles = user.getRoles();
+
+        roles.forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+            role.getPermissions().forEach(
+                    permission -> authorities.add(new SimpleGrantedAuthority(permission.getName()))
+            );
+        });
+
+        return authorities;
+    }
+
+    public UUID getId() {
+        return user.getId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+}
