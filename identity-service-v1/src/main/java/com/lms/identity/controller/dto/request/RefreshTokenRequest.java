5f990449e82b332d74627748d9fbc58f3a865661
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-20 14:46:12 -0500

    feat(auth): add login and registration request DTOs

---
+++ identity-service-v1/src/main/java/com/lms/identity/controller/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,16 @@
+package com.lms.identity.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+
+    @NotBlank(message = "Refresh token is required")
+    private String refreshToken;
+
+}
