e46cde47c39a605de13545add3d24606a66676a9
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-21 20:31:44 -0500

    feat(course-exception): add custom exceptions for resource handling

---
+++ course-service-v1/src/main/java/com/lms/course/exception/DuplicateResourceException.java
@@ -0,0 +1,9 @@
+package com.lms.course.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+}
---
+++ course-service-v1/src/main/java/com/lms/course/exception/InvalidOperationException.java
@@ -0,0 +1,9 @@
+package com.lms.course.exception;
+
+public class InvalidOperationException extends RuntimeException {
+
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+
+}
