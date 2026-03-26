434e315506dfa1c56cc094f56d1e9eadb88353f3
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-25 20:38:36 -0500

    feat(enrollment-service-v1-exception): add custom exceptions for resource handling

---
+++ enrollment-service-v1/src/main/java/com/lms/enrollment/exception/InvalidOperationException.java
@@ -0,0 +1,9 @@
+package com.lms.enrollment.exception;
+
+public class InvalidOperationException extends RuntimeException {
+
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+
+}
