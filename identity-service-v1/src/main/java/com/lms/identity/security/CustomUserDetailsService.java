eff2dcbc2ceee8897d8da28cf6dc7c9acb5b3f0f
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-04-06 11:36:47 -0500

    fix(auth): change exception type for user not found in loadUserByUsername

--- identity-service-v1/src/main/java/com/lms/identity/security/CustomUserDetailsService.java
+++ identity-service-v1/src/main/java/com/lms/identity/security/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.lms.identity.security;
 
-import com.lms.identity.exception.ResourceNotFoundException;
 import com.lms.identity.infrastructure.persistence.entity.UserEntity;
 import com.lms.identity.infrastructure.persistence.repository.JpaUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + email));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
         return new CustomUserDetails(user);
     }
 
